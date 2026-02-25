b815e514f2870bd2389c2fd6a3eada171b65fa5b
Timur Karimov <tkarimov@academic.rrc.ca>
2026-02-24 19:34:59 -0600

    update smtg

--- src/api/v1/routes/productRoutes.ts
+++ src/api/v1/routes/productRoutes.ts
@@ -12,13 +12,21 @@ import {
   updateProductSchema
 } from "../validation/productValidation";
 
-export const productRoutes = Router();
+export const productRoutes: Router = Router();
 
 productRoutes.get("/products", getAllProductsController);
 productRoutes.get("/products/:id", getProductByIdController);
 
-productRoutes.post("/products", validateRequest(createProductSchema), createProductController);
+productRoutes.post(
+  "/products",
+  validateRequest(createProductSchema),
+  createProductController
+);
 
-productRoutes.put("/products/:id", validateRequest(updateProductSchema), updateProductController);
+productRoutes.put(
+  "/products/:id",
+  validateRequest(updateProductSchema),
+  updateProductController
+);
 
 productRoutes.delete("/products/:id", deleteProductController);
