16d0323f7d3429a71b92d255c5c6322243878c14
Timur Karimov <tkarimov@academic.rrc.ca>
2026-02-24 18:00:43 -0600

    add func for read product   service

--- src/api/v1/controllers/productController.ts
+++ src/api/v1/controllers/productController.ts
@@ -10,3 +10,14 @@ import { CreateProductInput, Product, UpdateProductInput } from "../models/produ
 const productsCollectionName: string = "products";
 
 type NewProductRecord = Omit<Product, "id">;
+
+
+export async function getAllProducts(): Promise<Product[]> {
+  const products: Product[] = await getAllDocuments<Product>(productsCollectionName);
+  return products;
+}
+
+export async function getProductById(productId: string): Promise<Product | null> {
+  const product: Product | null = await getDocumentById<Product>(productsCollectionName, productId);
+  return product;
+}
