a9d7087fa2ef350cdea5b2b962ff81cde450667f
Timur Karimov <tkarimov@academic.rrc.ca>
2026-02-24 18:34:13 -0600

    add Routes for product

---
+++ src/api/v1/routes/productRoutes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import {
+  createProductController,
+  deleteProductController,
+  getAllProductsController,
+  getProductByIdController,
+  updateProductController
+} from "../controllers/productController";
+import { validateRequest } from "../middleware/validateRequest";
+import { createProductSchema, updateProductSchema } from "../validation/productValidation";
+
+export const productRoutes: Router = Router();
+
+productRoutes.get("/products", getAllProductsController);
+productRoutes.get("/products/:id", getProductByIdController);
+
+productRoutes.post(
+  "/products",
+  validateRequest(createProductSchema),
+  createProductController
+);
+
+productRoutes.put(
+  "/products/:id",
+  validateRequest(updateProductSchema),
+  updateProductController
+);
+
+productRoutes.delete("/products/:id", deleteProductController);
