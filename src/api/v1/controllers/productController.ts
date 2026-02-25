d0870516c41f7284fd498e46fa0295396a3082b7
Timur Karimov <tkarimov@academic.rrc.ca>
2026-02-24 18:01:21 -0600

    add create product service function

--- src/api/v1/controllers/productController.ts
+++ src/api/v1/controllers/productController.ts
@@ -21,3 +21,24 @@ export async function getProductById(productId: string): Promise<Product | null>
   const product: Product | null = await getDocumentById<Product>(productsCollectionName, productId);
   return product;
 }
+
+export async function createProduct(productInput: CreateProductInput): Promise<Product> {
+  const now: Date = new Date();
+
+  const newProductRecord: NewProductRecord = {
+    name: productInput.name,
+    sku: productInput.sku,
+    quantity: productInput.quantity,
+    price: productInput.price,
+    category: productInput.category,
+    createdAt: now,
+    updatedAt: now
+  };
+
+  const createdProduct: Product = await createDocument<NewProductRecord>(
+    productsCollectionName,
+    newProductRecord
+  );
+
+  return createdProduct;
+}
