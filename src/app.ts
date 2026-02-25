3040549c42938d2597aefadb8e8a13e547509fad
Timur Karimov <tkarimov@academic.rrc.ca>
2026-02-24 19:29:04 -0600

    update routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import { healthRoutes } from "./api/v1/routes/healthRoutes";
+import { productRoutes } from "./api/v1/routes/productRoutes";
 
 
 export const app = express();
@@ -9,4 +10,4 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1", healthRoutes);
-
+app.use("/api/v1/productRoutes", productRoutes);
